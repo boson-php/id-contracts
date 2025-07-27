e8f2060060bc9dd9ac1ba0ea39027b179af8077c
SerafimArts <nesk@xakep.ru>
2025-07-27 16:06:16 +0000

    Add ID contracts package

---
+++ src/IntIdInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Contracts\Id;
+
+use Boson\Contracts\ValueObject\IntValueObjectInterface;
+
+/**
+ * Representation of all int-like identifiers
+ *
+ * Note: All {@see int} identifiers also supports {@see string}
+ *       representation features
+ *
+ * @template-covariant T of int = int
+ *
+ * @template-extends IntValueObjectInterface<T>
+ */
+interface IntIdInterface extends
+    IntValueObjectInterface,
+    IdInterface {}
---
+++ tests/CompatibilityTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Contracts\Id\Tests;
+
+use Boson\Contracts\Id\IdInterface;
+use Boson\Contracts\Id\StringIdInterface;
+use Boson\Contracts\Id\IntIdInterface;
+use Boson\Contracts\Id\IdentifiableInterface;
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Note: Changing the behavior of these tests is allowed ONLY when updating
+ *       a MAJOR version of the package.
+ */
+#[Group('boson-php/id-contracts')]
+final class CompatibilityTest extends TestCase
+{
+    #[DoesNotPerformAssertions]
+    public function testIdInterfaceCompatibility(): void
+    {
+        new class implements IdInterface {
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testStringIdInterfaceCompatibility(): void
+    {
+        new class implements StringIdInterface {
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testIntIdInterfaceCompatibility(): void
+    {
+        new class implements IntIdInterface {
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+
+            public function toInteger(): int {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testIdentifiableInterfaceCompatibility(): void
+    {
+        new class implements IdentifiableInterface {
+            public IdInterface $id {
+                get {}
+            }
+        };
+    }
+}
